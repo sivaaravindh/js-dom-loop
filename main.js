663626f083d51d1e19776e8a31fd840a16a0bb4e
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-29 09:18:25 +0530

    index

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let head2=document.getElementsByName("fname")
+
+console.log(head2[0].innerText);
+
+let  head3= document.getElementsByClassName("logo")
+
+console.log(head3[0].innerText);
+// console.log(head3[0].tagName);
+
+let  head4 = document.getElementsByTagName("button")
+
+console.log(head4[0].innerText);
